5f66340fd1b196251bc7d1c48ba23380b7e6b278
Amos Wong <amoswwj@gmail.com>
2024-02-10 17:19:28 +0800

    ci: setup build public path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BUILD_PUBLIC_PATH = process.env.BUILD_PUBLIC_PATH || '';
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: `${BUILD_PUBLIC_PATH}/dist/`,
   plugins: [react()],
 })
