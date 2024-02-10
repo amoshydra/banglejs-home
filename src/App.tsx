37f8712695fbf33007adda6271c9d67f782aadf7
Amos Wong <amoswwj@gmail.com>
2024-02-11 03:15:10 +0800

    refactor: move api request and sort logic into method file

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,16 @@
+import { useState } from 'react';
 import './App.css'
+import { BangleJsAppFilters, BangleJsAppSortType, useApps } from './api/banglejs/methods';
 import { AppListItem } from './components/AppListItem';
-import { getAppDates, getAppUsage, getApps } from './api/banglejs/request';
-import useSWRImmutable from "swr/immutable";
 import { css } from '@emotion/react';
 
-const useApps = (sortedBy: "favourited" | "installed" | "modified" | "created") => {
-  const responseApps = useSWRImmutable("/apps", getApps);
-  const responseAppUsage = useSWRImmutable("/usage", getAppUsage);
-  const responseAppDates = useSWRImmutable("/dates", getAppDates);
-
-  const isLoading = (
-    responseApps.isLoading ||
-    responseAppUsage.isLoading ||
-    responseAppDates.isLoading
-  );
-  const error = (
-    responseApps.error ||
-    responseAppUsage.error ||
-    responseAppDates.error ||
-    null
-  );
-
-  if (isLoading || error) {
-    return {
-      isLoading,
-      error,
-      data: null,
-    };
-  }
-
-  const apps = responseApps.data!.items.slice();
-
-  if (sortedBy === "favourited" || sortedBy === "installed") {
-    const sortKey = sortedBy === "favourited" ? "fav" : "app";
-
-    const favouriteManifest = responseAppUsage.data![sortKey];
-    return {
-      isLoading,
-      error,
-      data: apps.sort((appA, appB) => {
-        return favouriteManifest[appB.id] - favouriteManifest[appA.id]
-      }),
-    }
-  }
-
-  const sortKey = sortedBy === "modified" ? "modifiedDate" : "createdDate";
-
-  const datesManifest = responseAppDates.data!;
-  console.log({datesManifest})
-  return {
-    isLoading,
-    error,
-    data: apps.sort((appA, appB) => {
-      return (
-        Date.parse(datesManifest[appB.id][sortKey]) -
-        Date.parse(datesManifest[appA.id][sortKey])
-      );
-    }),
-  };
-}
-
 function App() {
-  const { data: apps, error, isLoading } = useApps("modified");
+  const [ sortedBy, setSortedBy ] = useState<BangleJsAppSortType>("modified");
+  const [ filters, setFilters ] = useState<BangleJsAppFilters>({});
+  const { data: apps, error, isLoading } = useApps({
+    sortedBy,
+    filters,
+  });
 
   if (isLoading) {
     return <div>Fetching store...</div>
---
+++ src/api/banglejs/methods.ts
@@ -0,0 +1,87 @@
+import { AppItem } from './interface';
+import { getAppDates, getAppUsage, getApps } from './request';
+import useSWRImmutable from "swr/immutable";
+
+export type BangleJsAppSortType = (
+  | "favourited"
+  | "installed"
+  | "modified"
+  | "created"
+);
+export type BangleJsAppFilters = {
+  [V in keyof AppItem]?: RegExp;
+}
+
+export interface UseAppsOptions {
+  sortedBy: BangleJsAppSortType,
+  filters: BangleJsAppFilters,
+}
+
+export const useApps = (options: UseAppsOptions) => {
+  const appFetchInfo = useSortedApps(options.sortedBy);
+
+  const filteredApps = appFetchInfo.data?.filter(() => {
+    return true;
+  });
+
+  return {
+    ...appFetchInfo,
+    data: filteredApps,
+  };
+};
+
+const useSortedApps = (sortedBy: BangleJsAppSortType) => {
+  const responseApps = useSWRImmutable("/apps", getApps);
+  const responseAppUsage = useSWRImmutable("/usage", getAppUsage);
+  const responseAppDates = useSWRImmutable("/dates", getAppDates);
+
+  const isLoading = (
+    responseApps.isLoading ||
+    responseAppUsage.isLoading ||
+    responseAppDates.isLoading
+  );
+  const error = (
+    responseApps.error ||
+    responseAppUsage.error ||
+    responseAppDates.error ||
+    null
+  );
+
+  if (isLoading || error) {
+    return {
+      isLoading,
+      error,
+      data: null,
+    };
+  }
+
+  const apps = responseApps.data!.items.slice();
+
+  if (sortedBy === "favourited" || sortedBy === "installed") {
+    const sortKey = sortedBy === "favourited" ? "fav" : "app";
+
+    const favouriteManifest = responseAppUsage.data![sortKey];
+    return {
+      isLoading,
+      error,
+      data: apps.sort((appA, appB) => {
+        return favouriteManifest[appB.id] - favouriteManifest[appA.id]
+      }),
+    }
+  }
+
+  const sortKey = sortedBy === "modified" ? "modifiedDate" : "createdDate";
+
+  const datesManifest = responseAppDates.data!;
+  console.log({datesManifest})
+  return {
+    isLoading,
+    error,
+    data: apps.sort((appA, appB) => {
+      return (
+        Date.parse(datesManifest[appB.id][sortKey]) -
+        Date.parse(datesManifest[appA.id][sortKey])
+      );
+    }),
+  };
+}
