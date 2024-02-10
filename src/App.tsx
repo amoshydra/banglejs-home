e929756359977715bb243d01401fde829d428ff2
Amos Wong <amoswwj@gmail.com>
2024-02-10 16:58:56 +0800

    feat: test connection to banglejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    fetch("https://banglejs.com/apps/apps.json")
+      .then(async (res) => {
+        const result = await res.json();
+        console.log(result);
+      })
+    ;
+  }, [])
 
   return (
     <>
