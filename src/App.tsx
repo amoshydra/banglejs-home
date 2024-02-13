b4762df7ff750c19723fee245f55e460fe709ebb
Amos Wong <amoswwj@gmail.com>
2024-02-13 23:39:23 +0800

    feat: cache response for app list and app details

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import {
   redirect,
 } from "react-router-dom";
 import App from './views/Apps';
+import { SWRConfig } from 'swr';
 
 const router = createHashRouter(
   createRoutesFromElements(
@@ -34,8 +35,59 @@ const router = createHashRouter(
 
 function Entry() {
   return (
-    <RouterProvider router={router} />
+    <SWRConfig value={{ provider: localStorageProvider }}>
+      <RouterProvider router={router} />
+    </SWRConfig>
   )
 }
 
 export default Entry
+
+interface CacheStorage {
+  caches: [string, any][];
+  created: number;
+}
+const CACHE_KEY = "app-cache"
+const ONE_DAY = (
+  1 // ms
+  * 1000 // ss
+  * 60 // mm
+  * 60 // hh
+  * 24
+)
+const DEFAULT_CACHE = JSON.stringify({
+  caches: [] as [string, any][],
+  created: 0,
+});
+function localStorageProvider() {
+  const working = {
+    map: new Map<string, any>(),
+    created: Date.now(),
+  };
+
+  try {
+    // When initializing, we restore the data from `localStorage` into a map.
+    const { caches, created } = JSON.parse(localStorage.getItem(CACHE_KEY) || DEFAULT_CACHE) as CacheStorage;
+    if (Date.now() - created < ONE_DAY) {
+      // restore if cache was created in less than a day
+      working.map = new Map(caches);
+      working.created = created;
+    } else {
+      // using empty cache map
+    }
+  } catch (e) {
+    // using empty cache map
+  }
+
+  // Before unloading the app, we write back all the data into `localStorage`.
+  window.addEventListener('beforeunload', () => {
+    const cacheStorage: CacheStorage = {
+      caches: [...working.map.entries()],
+      created: working.created,
+    };
+    localStorage.setItem(CACHE_KEY, JSON.stringify(cacheStorage))
+  })
+
+  // We still use the map for write & read for performance.
+  return working.map;
+}
