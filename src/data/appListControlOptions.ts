bd0570a7a8ec8bcecc97a283b30071c43350141c
Amos Wong <amoswwj@gmail.com>
2024-02-14 15:57:58 +0800

    feat: add filters for app.type

--- src/data/appListControlOptions.ts
+++ src/data/appListControlOptions.ts
@@ -1,4 +1,5 @@
 import { AppItem } from "../api/banglejs/interface";
+import { pick, capitalize } from "lodash/fp";
 
 export const sortControl = {
   key: "__sortedBy",
@@ -23,6 +24,35 @@ export const filterControlMap = {
         { label: "all", value: () => true, default: true },
         { label: "BangleJS 1", value: (v: AppItem) => v.supports.includes('BANGLEJS')},
         { label: "BangleJS 2", value: (v: AppItem) => v.supports.includes('BANGLEJS2')},
+      ],
+    }
+  },
+  tags: {
+    label: "Type",
+    inputMethod: {
+      type: "radio" as const,
+      options: [
+        { label: "All", value: () => true, default: true },
+        ...(
+          [
+            "widget",
+            "clock",
+            "app",
+            "bootloader",
+            "RAM",
+            "module",
+            "clkinfo",
+            "launch",
+            "textinput",
+            "settings",
+            "locale",
+            "notify",
+            "scheduler",
+          ].map(type => ({
+            label: capitalize(type),
+            value: (app: AppItem) => app.type?.toLowerCase() === type.toLowerCase(),
+          }))
+        ),
       ],
     }
   },
