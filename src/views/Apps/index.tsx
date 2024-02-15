28ca420ccc358536f24dffa51ca5dd15f9e543bc
Amos Wong <amoswwj@gmail.com>
2024-02-16 04:00:30 +0800

    feat: change banglejs variant on device connection

--- src/views/Apps/index.tsx
+++ src/views/Apps/index.tsx
@@ -6,14 +6,24 @@ import { css } from '@emotion/react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { AppListItemDetailView } from '../../components/AppListItemDetailView/AppListItemDetailView';
 import { useAppListControl } from '../../hooks/appListControl';
+import { useEspruinoDeviceInfoStore } from '../../services/Espruino/stores/EspruinoDevice';
+import { filterControlMap } from '../../data/appListControlOptions';
 
 function App() {
+  const device = useEspruinoDeviceInfoStore(state => state.device);
   const { filters, setFilter, sortedBy, setSortedBy } = useAppListControl();
   const { data, error, isLoading } = useApps({
     sortedBy,
     filters,
   });
 
+  if (device && device.id) {
+    const foundOption = filterControlMap.supports.inputMethod.options.find(option => option.label.replace(/ /g, '').toUpperCase() === device.id)?.value;
+    if (foundOption && filters.supports !== foundOption) {
+      setFilter("supports", foundOption);
+      console.log("setting");
+    }
+  }
 
   const { appId } = useParams();
   const navigate = useNavigate();
