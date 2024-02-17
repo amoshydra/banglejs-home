2e4762511d41a228e2d70779cdffe5d5bff6da9f
Amos Wong <amoswwj@gmail.com>
2024-02-18 06:57:29 +0800

    feat: use app assets from submodules
    this ensure everything including iframe url are in the same domain

--- src/api/banglejs/urls.ts
+++ src/api/banglejs/urls.ts
@@ -1,11 +1,13 @@
 
-export const BaseUrl = "https://banglejs.com/apps";
+export const BaseUrl = `${location.origin}${import.meta.env.BASE_URL}externals/espruino/BangleApps`;
+export const RemoteBaseUrl = 'https://banglejs.com/apps';
 
 const fromBaseUrl = (path: string) => `${BaseUrl}${path}`;
+const fromRemoteBaseUrl = (path: string) => `${RemoteBaseUrl}${path}`;
 
 export const apps = () => fromBaseUrl('/apps.json');
 
-export const appUsage = () => fromBaseUrl('/appusage.json');
+export const appUsage = () => fromRemoteBaseUrl('/appusage.json');
 export const appDates = () => fromBaseUrl('/appdates.csv');
 
 const PLACEHOLDER_IMAGE = "data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==";
