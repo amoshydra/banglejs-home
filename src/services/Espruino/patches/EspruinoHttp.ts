2b0b3d77b76034b2a16fc30c358a3fb5dd476846
Amos Wong <amoswwj@gmail.com>
2024-02-18 08:41:25 +0800

    fix: validate url before fetching for Espruino's http utils

--- src/services/Espruino/patches/EspruinoHttp.ts
+++ src/services/Espruino/patches/EspruinoHttp.ts
@@ -1,10 +1,12 @@
+import * as BangleJsUrls from "../../../api/banglejs/urls";
+
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const espruinoOriginalHttpGet = httpGet;
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 window.httpGet = (url: string) => {
-  const updatedUrl = new URL(url, "https://banglejs.com/apps/");
+  const updatedUrl = new URL(url, BangleJsUrls.BaseUrl);
   return espruinoOriginalHttpGet(updatedUrl.href);
 }
 
@@ -29,8 +31,42 @@ const originalUtils: (typeof Espruino)["Core"]["Utils"] = {
 
 const patchUrl = (methodName: keyof typeof originalUtils) => {
   Espruino.Core.Utils[methodName] = (href: string, ...others) => {
-    const newHref = href.replace(location.origin + import.meta.env.BASE_URL, "https://banglejs.com/apps/")
-    originalUtils[methodName](newHref, ...others);
+    const baseUrl = import.meta.env.BASE_URL;
+    const originalRoot = location.origin + baseUrl;
+    const newHref = href.replace(originalRoot, BangleJsUrls.BaseUrl);
+
+    let promise = Promise.resolve(newHref);
+
+    // validate url before passing it to Espruino http request function
+    //
+    // Request made to a Single Page Application does not always respond with a 404 when a file is not found.
+    // Many server configure a 404 fallback to the index.html.
+    // This logic perform a check by pre-fetching the URL first before passing it to Espruino's HTTP utils.
+    //
+    // A response with content-type = text/html from the same origin likely imply an errorneous response.
+    //
+    // ^ Side note: GitHub page does return 404, so this only happen in development mode for now
+    if (new URL(newHref).origin === location.origin) {
+      promise = promise
+        .then((url) => 
+          fetch(url, {
+            method: "head",
+          })
+          .then((res) => {
+            if (!res.ok) return url;
+
+            if (res.headers.get("content-type")?.split(";").map(s => s.trim()).includes("text/html")) {
+              const invalidUrl = new URL("https://intentionally-invalid-domain")
+              invalidUrl.hash = url;
+              return invalidUrl.href;
+            }
+            return url;
+          })
+        )
+    }
+    return promise.then((url) => {
+      return originalUtils[methodName](url, ...others);
+    });
   }
 }
 
