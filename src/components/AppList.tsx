bbc56ba59f242c140bda33a2fee3b7315fe40681
Amos Wong <amoswwj@gmail.com>
2024-02-18 16:08:32 +0800

    feat: render AppList with react-virtualized

--- src/components/AppList.tsx
+++ src/components/AppList.tsx
@@ -1,16 +1,52 @@
 import { css } from "@emotion/react";
 import { AppItem } from "../api/banglejs/interface";
 import { AppListItem } from "./AppListItem";
-import { HTMLAttributes } from "react";
+import { CSSProperties, HTMLAttributes,  useLayoutEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { ListRowRenderer, List } from 'react-virtualized/dist/es/List';
 
-export interface AppListProps extends HTMLAttributes<HTMLDivElement> {
+export interface AppListProps {
   isLoading: boolean;
   error: Error | null;
-  data: AppItem[]
+  data: AppItem[];
+  className?: string;
+  style?: CSSProperties;
 }
 
+const getDimension = (element: HTMLElement) => {
+  return {
+    height: element.parentElement!.clientHeight,
+    width: element.parentElement!.clientWidth,
+  }
+};
+
 export const AppList = ({ isLoading, error, data: apps, ...props }: AppListProps) => {
+  const [ dimension, setDimension ] = useState({ width: 0, height: 0 });
+  const ref = useRef<HTMLDivElement>(null);
+
+  useLayoutEffect(() => {
+    const element = ref.current;
+    const resizeHandler = () => {
+      if (element) {
+        setDimension(getDimension(element))
+      }
+    };
+
+    const observer = new ResizeObserver(() => {
+      resizeHandler()
+    });
+    resizeHandler();
+
+    if (element) observer.observe(element);
+
+    return () => {
+      if (element) {
+        observer.unobserve(element);
+        observer.disconnect();
+      }
+    }
+  }, [])
+
 
   if (isLoading) {
     return <MessageWrapper>Fetching store...</MessageWrapper>
@@ -19,34 +55,45 @@ export const AppList = ({ isLoading, error, data: apps, ...props }: AppListProps
     return <MessageWrapper>Error fetching store, please try again later...</MessageWrapper>
   }
 
+  const RowRenderer: ListRowRenderer = (p) => {
+    const app = apps[p.index];
+  
+    return (
+      <Link
+        key={p.key}
+        style={p.style}
+        to={`/apps/${app.id}`}
+        css={css`
+          color: inherit;
+          &:hover {
+            color: inherit;
+          }
+        `}
+      >
+        <AppListItem
+          key={app.id}
+          app={app}
+        />
+      </Link>
+    );
+  }
+
   return (
     <div
-      {...props}
+      ref={ref}
       css={css`
-        display: flex;
-        flex-direction: column;
+        height: 100%;
       `}
     >
-      {
-        apps!.map(app => {
-          return (
-            <Link
-              to={`/apps/${app.id}`}
-              css={css`
-                color: inherit;
-                &:hover {
-                  color: inherit;
-                }
-              `}
-            >
-              <AppListItem
-                key={app.id}
-                app={app}
-              />
-            </Link>
-          )
-        })
-      }
+      <List
+        {...props}
+        aria-readonly={undefined}
+        width={dimension.width}
+        height={dimension.height}
+        rowCount={apps.length}
+        rowHeight={126}
+        rowRenderer={RowRenderer}
+      />
     </div>
   )
 };
