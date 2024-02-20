9421d8da78ffa93ef72601c0a18adcd7f14e114c
Amos Wong <amoswwj@gmail.com>
2024-02-20 09:34:26 +0800

    feat: show app list

--- src/components/AppListControlsHeader/DeviceControl/DeviceView.tsx
+++ src/components/AppListControlsHeader/DeviceControl/DeviceView.tsx
@@ -8,9 +8,20 @@ export const DeviceView = (props: DeviceViewProps) => {
   return (
     <div className={props.className}>
       <h2>Device Info</h2>
+
+      <br />
+      <div>
+        {props.device.appsInstalled.map((app) => (
+          <div key={app.id}>{ app.id }</div>
+        ))}
+      </div>
+
+      <br />
+
       <pre>
         {JSON.stringify(props.device, null, 2)}
       </pre>
+
     </div>
   )
 };
